e6cfae0597ed34fe72a7cd817ee3016ad14c1821
Ch3did <pedro.chedid@200dev.com>
2023-07-03 12:18:41 -0300

    feat: created section

--- src/helpers/get_env.py
+++ src/helpers/get_env.py
@@ -10,6 +10,8 @@ DEBUG = os.environ.get("DEBUG")
 # Excel File Name
 FILE_NAME = os.environ.get("FILE_NAME")
 
+# category new's
+SECTION =  os.environ.get("SECTION")
 
 # URL Views
 NYT_URL = os.environ.get("NEW_YOUR_TIMES")
