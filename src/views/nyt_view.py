6a4e3362044923bccb918192bead4f46d4805d96
Ch3did <pedro.chedid@200dev.com>
2023-07-03 10:10:30 -0300

    feat: update view

--- src/views/nyt_view.py
+++ src/views/nyt_view.py
@@ -1,6 +1,7 @@
 import time
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 from src.helpers.get_env import NYT_URL
 
@@ -8,15 +9,15 @@ from src.helpers.get_env import NYT_URL
 class NYTView:
     def __init__(self) -> None:
         self.url = f"{NYT_URL}"
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Chrome()
 
         
     def run(self) -> dict:
         self.driver.get(self.url)
         time.sleep(3)
-        
-        
-        
+        self.driver.set_window_size(1240, 981)
+        value = self.driver.find_elements(By.XPATH, "")
+
         
         data_dict = {}
         return data_dict
