33c11998dd66d7cf1e39482e20563905cf6bd3aa
Ch3did <pedro.chedid@200dev.com>
2023-07-03 14:34:58 -0300

    feat: solved filter logic

--- src/helpers/xpath.py
+++ src/helpers/xpath.py
@@ -1,8 +1,32 @@
 class XpathNotations:
     @classmethod
-    def get_categories(self) -> str:
-        return '//*[@id="app"]/div[2]/div/header/div[4]/ul/li'
+    def get_date_btn(self) -> str:
+        return (
+            '//*[@id="site-content"]/div[1]/div[1]/div[2]/div/div/div[1]/div/div/button'
+        )
 
     @classmethod
-    def get_category_element(self, element):
-        return f'//*[@id="app"]/div[2]/div/header/div[4]/ul/li[{element+1}]/a'
+    def get_btn_year(self):
+        return '//*[@id="site-content"]/div[1]/div[1]/div[2]/div/div/div[1]/div/div/div/ul/li[5]/button'
+
+    @classmethod
+    def get_btn_section(self):
+        return (
+            '//*[@id="site-content"]/div[1]/div[1]/div[2]/div/div/div[2]/div/div/button'
+        )
+
+    @classmethod
+    def get_section_list(self):
+        return '//*[@id="site-content"]/div[1]/div[1]/div[2]/div/div/div[2]/div/div/div/ul/li'
+
+    @classmethod
+    def get_section(self, index):
+        return f'//*[@id="site-content"]/div[1]/div[1]/div[2]/div/div/div[2]/div/div/div/ul/li[{index}]/label'
+
+    @classmethod
+    def get_search_field(self):
+        return '//*[@id="searchTextField"]'
+
+    @classmethod
+    def get_search_btn(self):
+        return '//*[@id="site-content"]/div/div[1]/div[1]/form/div[1]/button/svg'
--- src/views/nyt_view.py
+++ src/views/nyt_view.py
@@ -4,42 +4,76 @@ from loguru import logger
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-from src.helpers.get_env import NYT_URL, SECTION
+from src.helpers.get_env import NYT_URL, PHRASE, SECTION
 from src.helpers.xpath import XpathNotations
 
 
 class NYTView:
     def __init__(self) -> None:
-        self.url = f"{NYT_URL}"
+        url = f"{NYT_URL}"
         self.driver = webdriver.Chrome()
         self.xpath = XpathNotations()
 
-    def select_section(self):
-        categories = self.driver.find_elements(By.XPATH, self.xpath.get_categories())
+        # Start ChromeDrive
+        self.driver.get(url)
 
-        categories_name = [item.text for item in categories]
+        # Set window size
+        self.driver.set_window_size(1240, 981)
+        self.driver.maximize_window()
+
+    def select_date_range(self) -> None:
+        "Select a year range news for filter results"
+        date_btn = self.driver.find_element(By.XPATH, self.xpath.get_date_btn())
+        date_btn.click()
+
+        a_year = self.driver.find_element(By.XPATH, self.xpath.get_btn_year())
+        a_year.click()
+
+        time.sleep(1)
+        date_btn.click()
+
+    def select_section(self) -> None:
+        "Uses section varible to filter results"
+        section_btn = self.driver.find_element(By.XPATH, self.xpath.get_btn_section())
+        section_btn.click()
+
+        section_list = self.driver.find_elements(
+            By.XPATH, self.xpath.get_section_list()
+        )
+
+        sections_name = [item.text for item in section_list]
+
         section = SECTION.title()
 
-        if section in categories_name:
-            index = categories_name.index(section)
-            el = self.driver.find_element(
-                By.XPATH, self.xpath.get_category_element(index)
+        if section in sections_name:
+            index = sections_name.index(section)
+            selected_section = self.driver.find_element(
+                By.XPATH, self.xpath.get_section(index + 1)
             )
-            el.click()
+            time.sleep(1)
+            selected_section.click()
 
         else:
             logger.error(f"Category {section} Not Found!!!")
 
-    def extract_data(self):
-        pass
+        section_btn.click()
+
+    def uses_search(self):
+        phrase = PHRASE.lower()
+
+        search_filed = self.driver.find_element(By.XPATH, self.xpath.get_search_field())
+        search_filed.send_keys(f"{phrase}")
+
+        search_btn = self.driver.find_element(By.XPATH, self.xpath.get_search_btn())
+        search_btn.click()
 
     def run(self) -> dict:
         data_dict = {}
-        self.driver.get(self.url)
-        self.driver.set_window_size(1240, 981)
-        self.driver.maximize_window()
 
+        # Added filters
+        self.select_date_range()
         self.select_section()
+        self.uses_search()
 
         data = self.extract_data()
 
