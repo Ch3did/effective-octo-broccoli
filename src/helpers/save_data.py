cbb71ce60147e29cf62007cd45330d312387c795
Ch3did <pedro.chedid@200dev.com>
2023-07-06 13:07:06 -0300

    fix: update file name

--- src/helpers/save_data.py
+++ src/helpers/save_data.py
@@ -1,19 +1,16 @@
 import pandas as pd
+from loguru import logger
 
-from src.helpers.get_env import FILE_NAME
+from src.helpers.get_env import FILE_NAME, MONTHS, PHRASE
 
 
 def append_data_to_excel(data_list):
-    file_path = f"tmp/{FILE_NAME}.xlsx"
+    file_path = f"tmp/{FILE_NAME}_{MONTHS}-months_{PHRASE}.xlsx"
 
-    # Create a DataFrame from the list of dictionaries
     df = pd.DataFrame(data_list)
-
-    # Create an Excel writer using pandas
     writer = pd.ExcelWriter(file_path, engine="xlsxwriter")
-
-    # Write the DataFrame to the Excel file
     df.to_excel(writer, index=False, sheet_name="Sheet1")
 
     # Save the Excel file
+    logger.info(f"Creating file {file_path[4:]}")
     writer.close()
