91c31074787a3482cf681b711c5233a63ad5f445
Ch3did <pedro.chedid@200dev.com>
2023-07-06 11:11:08 -0300

    feat: migrating to robocorp

--- tasks.py
+++ tasks.py
@@ -1,7 +1,8 @@
 from src.helpers.save import append_data_to_excel
 from src.views.nyt_view import NYTView
+from robocorp.tasks import task
 
-
+@task
 def start_extraction():
     raw_data = NYTView().run()
     append_data_to_excel(raw_data)
