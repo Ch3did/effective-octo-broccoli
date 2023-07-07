a815539415b935dae5c556561caf855a40a59b35
Ch3did <pedro.chedid@200dev.com>
2023-07-07 00:28:40 -0300

    feat: update get_env file

--- src/helpers/get_env.py
+++ src/helpers/get_env.py
@@ -2,7 +2,7 @@ import os
 
 from dotenv import load_dotenv
 
-load_dotenv(".env")
+load_dotenv("env")
 
 # Debug for loger
 DEBUG = os.environ.get("DEBUG")
@@ -10,16 +10,14 @@ DEBUG = os.environ.get("DEBUG")
 # Excel File Name
 FILE_NAME = os.environ.get("FILE_NAME")
 
-# category new's
-SECTION = os.environ.get("SECTION")
-
-# Phrase_variable
-PHRASE = os.environ.get("PHRASE")
-
-MONTHS = os.environ.get("MONTHS")
-
 # URL Views
 NYT_URL = os.environ.get("NEW_YOUR_TIMES")
 
-
+# Picture  folder
 PICTURE_PATH = os.environ.get("PICTURE_PATH")
+
+
+# category new's
+SECTION = os.environ.get("SECTION", "World")
+PHRASE = os.environ.get("PHRASE", "Brazil")
+MONTHS = os.environ.get("MONTHS", 0)
